07257a223c81bc6e22aaaf9b3c87b4b671c4c58e
Glomzzz <glom@skillw.com>
2025-06-19 04:36:41 +0800

    fix(retry replaced articles)

--- src/compile/compiler/cache/article.rs
+++ src/compile/compiler/cache/article.rs
@@ -4,29 +4,87 @@ use crate::compile::registry::{Key, KeyRegistry};
 use crate::config::TypsiteConfig;
 use crate::ir::article::{Article, PureArticle};
 use crate::util::error::{log_err, log_err_or_ok};
-use crate::util::fs::{remove_file_ignore, write_into_file};
+use crate::util::fs::{remove_file_ignore, remove_file_log_err, write_into_file};
 use crate::walk_glob;
 use anyhow::{Context, anyhow};
 use glob::glob;
 use rayon::prelude::*;
-use std::collections::hash_map::Drain;
 use std::collections::HashMap;
+use std::collections::hash_map::Drain;
 use std::path::{Path, PathBuf};
 use std::sync::Arc;
 
 pub struct ArticleCache<'a> {
     cache_article_path: PathBuf,
+    cache_html_path: PathBuf,
     cache: HashMap<Key, Article<'a>>,
 }
 
 impl<'a> ArticleCache<'a> {
     pub fn new(cache_path: &Path) -> ArticleCache<'a> {
         let cache_article_path = cache_path.join("article");
+        let cache_html_path = cache_path.join("html");
         Self {
             cache_article_path,
+            cache_html_path,
             cache: HashMap::new(),
         }
     }
+
+    fn typ_to_html_path(&self, path: &Path) -> PathBuf {
+        self.cache_html_path
+            .join(path)
+            .with_extension("html")
+    }
+    fn typ_to_json_path(&self, path: &Path) -> PathBuf {
+        self.cache_article_path
+            .join(path)
+            .with_extension("typ.json")
+    }
+
+    fn errors(
+        &mut self,
+        registry: &mut KeyRegistry,
+        error_articles: &mut HashMap<Key, (PathBuf, TypError)>,
+        failed: Vec<TypError>,
+    ) {
+        failed.into_iter().for_each(|err| {
+            let slug = err.slug.clone();
+            let path = registry.path(&slug).unwrap().to_path_buf();
+            registry.remove_slug(&slug);
+            self.cache.remove(&slug);
+            let cache = self.typ_to_json_path(&path);
+            remove_file_ignore(cache);
+            let html = self.typ_to_html_path(&path);
+            error_articles.insert(slug, (html, err));
+        });
+        let failed = self
+            .cache
+            .iter()
+            .filter_map(|(slug, article)| {
+                let errors: Vec<_> = error_articles
+                    .keys()
+                    .filter_map(|error_slug| {
+                        if article.get_depending_articles().contains(error_slug) {
+                            Some(anyhow!("Article {error_slug} not found"))
+                        } else {
+                            None
+                        }
+                    })
+                    .collect();
+                if errors.is_empty() {
+                    None
+                } else {
+                    Some(TypError::new_with(slug.clone(), errors))
+                }
+            })
+            .collect::<Vec<_>>();
+        let _ = failed.iter().filter_map(|err| self.cache.remove(&err.slug));
+        if !failed.is_empty() {
+            self.errors(registry, error_articles, failed);
+        }
+    }
+
     pub fn load(
         &mut self,
         config: &'a TypsiteConfig,
@@ -35,8 +93,8 @@ impl<'a> ArticleCache<'a> {
     ) -> ErrorArticles {
         deleted
             .into_par_iter()
-            .map(|path| path.with_extension("json"))
-            .for_each(|path| remove_file_ignore(&path));
+            .map(|path| self.typ_to_json_path(path))
+            .for_each(|path| remove_file_log_err(path, "delete while load cahce"));
 
         let pures = walk_glob!("{}/**/*.json", self.cache_article_path.display())
             .par_bridge()
@@ -72,45 +130,7 @@ impl<'a> ArticleCache<'a> {
             .collect::<Vec<_>>();
 
         let mut error_articles = HashMap::new();
-        fn errors(
-            cache: &mut HashMap<Key, Article<'_>>,
-            registry: &mut KeyRegistry,
-            error_articles: &mut HashMap<Key, (PathBuf, TypError)>,
-            failed: Vec<TypError>,
-        ) {
-            failed.into_iter().for_each(|err| {
-                let slug = err.slug.clone();
-                let path = registry.path(&slug).unwrap().to_path_buf();
-                registry.remove_slug(&slug);
-                cache.remove(&slug);
-                error_articles.insert(slug, (path, err));
-            });
-            let failed = cache
-                .iter()
-                .filter_map(|(slug, article)| {
-                    let errors: Vec<_> = error_articles
-                        .keys()
-                        .filter_map(|error_slug| {
-                            if article.get_depending_articles().contains(error_slug) {
-                                Some(anyhow!("Article {error_slug} not found"))
-                            } else {
-                                None
-                            }
-                        })
-                        .collect();
-                    if errors.is_empty() {
-                        None
-                    } else {
-                        Some(TypError::new_with(slug.clone(), errors))
-                    }
-                })
-                .collect::<Vec<_>>();
-            let _ = failed.iter().filter_map(|err| cache.remove(&err.slug));
-            if !failed.is_empty() {
-                errors(cache, registry, error_articles, failed);
-            }
-        }
-        errors(&mut self.cache, registry, &mut error_articles, failed);
+        self.errors(registry, &mut error_articles, failed);
         error_articles
             .into_iter()
             .map(|(_, (path, err))| (path, format!("{err}")))
@@ -143,7 +163,7 @@ impl<'a> ArticleCache<'a> {
         Ok(())
     }
 
-    pub fn get(&self,slug:&Arc<str>) -> Option<&Article<'a>> {
+    pub fn get(&self, slug: &Arc<str>) -> Option<&Article<'a>> {
         self.cache.get(slug)
     }
     pub fn drain(&mut self) -> Drain<'_, Key, Article<'a>> {
